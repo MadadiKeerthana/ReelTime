c6c2f9f22d078d777410ee6e316a6f51dc62fecf
Keerthana <48146190+MadadiKeerthana@users.noreply.github.com>
2026-09-18 21:14:35 -0400

    Save event to database

--- database.py
+++ database.py
@@ -1,7 +1,6 @@
 import sqlite3
 
 connection = sqlite3.connect("reeltime.db")
-
 cursor = connection.cursor()
 
 cursor.execute("""
@@ -16,5 +15,29 @@ cursor.execute("""
 """)
 
 connection.commit()
-
 connection.close()
+
+def save_event(event):
+    try:
+        with sqlite3.connect("reeltime.db") as connection:
+            cursor = connection.cursor()
+            cursor.execute("""
+                INSERT INTO viewing_events (
+                    event_id, 
+                    user_id, 
+                    title_id,
+                    genre,
+                    watch_seconds,
+                    timestamp) Values 
+                    (?, ?, ?, ?, ?, ?)""",
+                    (event.event_id, 
+                    event.user_id, 
+                    event.title_id, 
+                    event.genre,
+                    event.watch_seconds,
+                    event.timestamp.isoformat())
+            )  
+        return True
+    
+    except sqlite3.IntegrityError:
+        return False
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
+
+from database import save_event
 from models.viewing_event import ViewingEvent
 
 app = FastAPI()
 
 @app.post("/events")
 def create_event(event: ViewingEvent):
-    return event
+    saved = save_event(event)
+    return {"status": "saved"} if saved else {"status": "duplicate"}
